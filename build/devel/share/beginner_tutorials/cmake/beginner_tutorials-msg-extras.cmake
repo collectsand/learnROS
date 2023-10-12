13bbcc93fef68b43ac9f2f4b02fe486373d41074
ayaya <aayaya897@gmail.com>
2023-10-12 21:28:04 +0800

    delete some rubbish

--- build/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(beginner_tutorials_MESSAGE_FILES "/home/qwe/catkin_ws/src/beginner_tutorials/msg/smsg.msg")
-set(beginner_tutorials_SERVICE_FILES "")
--- build/devel/share/common-lisp/ros/beginner_tutorials/msg/_package_smsg.lisp
+++
@@ -1,8 +0,0 @@
-(cl:in-package beginner_tutorials-msg)
-(cl:export '(NAME-VAL
-          NAME
-          NUM-VAL
-          NUM
-          HEIGHT-VAL
-          HEIGHT
-))
