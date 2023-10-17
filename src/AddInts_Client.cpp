a4ecccc416ad9f73d4ca54ca16b44081b74190d5
ayaya <aayaya897@gmail.com>
2023-10-17 11:25:52 +0800

    server and client

---
+++ src/AddInts_Client.cpp
@@ -0,0 +1,30 @@
+#include "beginner_tutorials/AddTwoInts.h"
+#include "ros/ros.h"
+#include "ros/service_client.h"
+
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    ROS_INFO("the number of argument is wrong, please inpute two arguments!");
+    return 1;
+  }
+  ros::init(argc, argv, "AddInts_Client");
+
+  ros::NodeHandle nh;
+
+  ros::ServiceClient client =
+      nh.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+
+  beginner_tutorials::AddTwoInts srv;
+  // 自定义参数
+  srv.request.a = atoll(argv[1]);
+  srv.request.b = atoll(argv[2]);
+
+  if (client.call(srv)) {
+    ROS_INFO("Sum: %ld", (long int)srv.response.sum);
+  } else {
+    ROS_ERROR("Failed to call service add_two_ints");
+    return 1;
+  }
+
+  return 0;
+}
